d1016a515622455e98557234c78332670897ea8a
iheytang <dehui1012@qq.com>
2025-05-06 23:35:27 +0800

    fix: fix mask

--- web/src/lib/maskdata.ts
+++ web/src/lib/maskdata.ts
@@ -1,8 +1,8 @@
 export function maskDataForLlmApiKey(data: string) {
   // reserve first 8 and last 4 characters
   const length = data.length;
-  const mask = '*'.repeat(length - 8 - 4);
-  return data.slice(0, 8) + mask + data.slice(-4);
+  const mask = '*'.repeat(length - 4);
+  return data.slice(0, 4) + mask + data.slice(-4);
 }
 
 export function isMaybeSameMaskedLlmApiKey(original: string, masked: string) {
