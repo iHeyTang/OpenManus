703e7537dc98409c9211c0c69dde46d6b68c3130
iheytang <dehui1012@qq.com>
2025-05-15 12:00:48 +0800

    fix: mask in isMaybeSameMaskedLlmApiKey should be same as maskDataForLlmApiKey

--- web/src/lib/maskdata.ts
+++ web/src/lib/maskdata.ts
@@ -1,12 +1,12 @@
 export function maskDataForLlmApiKey(data: string) {
-  // reserve first 8 and last 4 characters
   const length = data.length;
   const mask = '*'.repeat(length - 4);
   return data.slice(0, 4) + mask + data.slice(-4);
 }
 
 export function isMaybeSameMaskedLlmApiKey(original: string, masked: string) {
-  return masked.slice(0, 8) === original.slice(0, 8) && masked.slice(-4) === original.slice(-4);
+  const maskedOriginal = maskDataForLlmApiKey(original);
+  return masked === maskedOriginal;
 }
 
 export function maskDataForFlatObject(env: Record<string, string>) {
